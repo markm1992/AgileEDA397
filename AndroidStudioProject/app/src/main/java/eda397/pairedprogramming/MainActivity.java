b1a3beb2926b591219f0007394b1525bac89360e
pukki <sebastian.pukki@gmail.com>
2015-04-20 08:33:58 +0200

    Removed test

--- AndroidStudioProject/app/src/main/java/eda397/pairedprogramming/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/eda397/pairedprogramming/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        System.out.println("test2");
-        System.out.println("test2");
         return true;
     }
 
